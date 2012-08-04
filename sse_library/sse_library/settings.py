13b4e7d6de1aff37905047ff527a037ee704e313
Morgan Cabral <morgan.cabral@gmail.com>
2012-08-04 19:41:09 -0400

    Added support for a local_settings.py file

--- sse_library/sse_library/settings.py
+++ sse_library/sse_library/settings.py
@@ -116,9 +116,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -149,3 +150,8 @@ LOGGING = {
         },
     }
 }
+
+try:
+    from local_settings import *
+except ImportError, exp:
+    pass
