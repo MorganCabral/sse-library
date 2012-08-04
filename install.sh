b6fe03902d7fb8741eefb804483d24dd6e6e7642
Morgan Cabral <morgan.cabral@gmail.com>
2012-08-04 05:33:05 -0400

    We get to see if the install script works now :D

--- install.sh
+++ install.sh
@@ -3,28 +3,19 @@
 # Grab the repo from github.
 git clone https://github.com/MorganCabral/sse-library.git
 
-# Make a directory for the project in the current directory. Then, CD into it.
+# Go into the project directory.
 cd sse-library
 
 # Setup the virtual environment we'll be using for development.
 # It doesn't particularly matter what you call this.
 virtualenv env
 
-# Activate the virtual environment.
-source env/bin/activate
-
-# Note: At this point you should see "(env)" somewhere in your
-# terminal input line thingy.
-
 # Note: At this point, there should be the project itself, an env directory and a file called requirements.txt.
 
 # Install project dependencies.
-pip install -r requirements.txt
+source env/bin/activate && pip install -r requirements.txt && deactivate
 
 # Note: If we go the PostgreSQL route, set up the db users and such here.
 
 # Run syncdb to setup the datebase schema.
-cd sse_library && ./manage.py syncdb
-
-# Run this if you want to get out of the virtual environment.
-deactivate
+source env/bin/activate && cd sse_library && ./manage.py syncdb && deactivate
