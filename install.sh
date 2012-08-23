dc0d748832d85682a2ff1e27236e74eb44d734d9
Nicolas McCurdy <thenickperson@gmail.com>
2012-08-23 01:08:22 -0400

    Merge both install scripts into install.sh (fix #4)

--- install.sh
+++ install.sh
@@ -1,5 +1,43 @@
 #!/bin/bash
 
+PM=false
+clear
+
+# Install the pre-reqs
+if command -v apt-get > /dev/null
+  then
+    echo "Installing prereqs with apt-get..."
+    sudo apt-get -y update
+    sudo apt-get -y install curl git python python-pip
+    $PM = true
+  fi
+if command -v yum > /dev/null
+  then
+    if $PM == false
+      then
+        echo "Installing prereqs with yum..."
+        sudo yum update
+        sudo yum -y install curl git python python-pip
+        $PM = true
+    fi
+  fi
+if command -v pacman >/dev/null
+  then
+    if $PM == false
+      then
+        echo "Installing prereqs with pacman..."
+        sudo pacman -S curl git python2 python2-pip
+        $PM = true
+    fi
+  fi
+
+# I beg of you, get a package manager!
+if $PM == false
+  then
+    echo "Install pre-reqs manually and use install.sh"
+    exit
+  fi
+
 # Grab the repo from github.
 git clone https://github.com/MorganCabral/sse-library.git
 
