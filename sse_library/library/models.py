a58e1f10a3a2ebc61edcf3aba5198ee840babb8e
Brian To <bxt5647@rit.edu>
2012-09-11 02:31:57 -0400

    Added documentation.

--- sse_library/library/models.py
+++ sse_library/library/models.py
@@ -2,22 +2,45 @@ from django.db import models
 from django.utils import timezone
 
 class Author(models.Model):
+  """ Author of a book.
+  
+      Relations:
+      * has many books
+  """
   name = models.CharField(max_length=254)
 
 class Publisher(models.Model):
+  """ Publisher of books.
+
+      Relations:
+      * has many books
+  """
   name = models.CharField(max_length=254)
 
 class Book(models.Model):
+  """ Book model.
+
+      Constants:
+      * DB_GOOD database shortand for a book in good condition
+      * DB_POOR database shortand for a book in poor condition
+      * CONDITIONS mapping from database shorthands to human-readable name
+
+      Relations:
+      * has many authors
+      * has one publisher
+  """
+
   DB_GOOD = '+'
   DB_POOR = '-'
   CONDITIONS = (
       (DB_GOOD, 'Good'),
       (DB_POOR, 'Poor'))
 
-  # Links
+  # Foreign Keys
   authors = models.ManyToManyField('Author', related_name='books')
   publisher = models.ForeignKey('Publisher', related_name='books')
 
+  # Fields
   title = models.CharField(max_length=254)
   isbn = models.CharField(max_length=16)
   publish_date = models.DateField(default=timezone.now())
