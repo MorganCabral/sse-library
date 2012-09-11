79bfad4f6322255b60d51cfd5f482a023fe7de87
Brian To <bxt5647@rit.edu>
2012-09-11 01:34:35 -0400

    Initial hash of library model.

--- sse_library/library/models.py
+++ sse_library/library/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Author(models.Model):
+  name = models.CharField(max_length=254)
+
+class Publisher(models.Model):
+  name = models.CharField(max_length=254)
+
+class Book(models.Model):
+  DB_GOOD = '+'
+  DB_POOR = '-'
+  CONDITIONS = (
+      (DB_GOOD, 'Good'),
+      (DB_POOR, 'Poor'))
+
+  # Links
+  authors = models.ManyToManyField('Author', related_name='books')
+  publisher = models.ForeignKey('Publisher', related_name='books')
+
+  title = models.CharField(max_length=254)
+  isbn = models.CharField(max_length=16)
+  publish_date = models.DateField(default=timezone.now())
+  condition = models.CharField(max_length=1, choices=CONDITIONS, default=DB_GOOD)
+  is_required_text = models.BooleanField()
